5d52990d04e13ad9dce99af10493b45ccc0ded63
sachinsapsap <sachinsapsap@gmail.com>
2024-06-17 13:14:34 +0530

    Third commit

--- src/main/java/com/qa/gorest/client/RestClient.java
+++ src/main/java/com/qa/gorest/client/RestClient.java
@@ -17,7 +17,7 @@ public class RestClient {
 
 //	private static final String BASE_URI = "https://gorest.co.in";
 //	private static final String BEARER_TOKEN = "e4b8e1f593dc4a731a153c5ec8cc9b8bbb583ae964ce650a741113091b4e2ac6";
-
+	
 	private RequestSpecBuilder specBuilder;
 
 	private Properties prop;
